28fa477ec1b3b4621f5c108032dd13c91b8925a7
Kurokishin <rafaelpereirasouza15@gmail.com>
2023-10-01 11:14:32 -0300

    route to cadastroPlaca

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, 'src/public')));
 
 // Usar o placaRouter para rota '/'
 app.use('/', homepageRoute);
-app.use('/cadastroPlaca', cadastroPlacaRouter);
+app.use('/', cadastroPlacaRouter);
 //app.use('/', relatorioPlacaRouter);
 //app.use('/', consultaPlacaRoute);
 
