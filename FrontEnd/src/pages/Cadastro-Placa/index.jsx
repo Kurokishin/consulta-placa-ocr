e9182d06c6a80ffa6b1e6bceb300b655e25ec145
Luiz Pedro Galdino Silva <luispedrogaldino53@gmail.com>
2023-10-02 13:15:08 -0300

    feat: Cadastro de nomes de cidades no json server

--- FrontEnd/src/pages/Cadastro-Placa/index.jsx
+++ FrontEnd/src/pages/Cadastro-Placa/index.jsx
@@ -1,32 +1,58 @@
 import { useState } from "react";
 import axios from "axios";
 import Styles from "../Cadastro-Placa/styles.module.css";
+import { Link } from "react-router-dom";
+import { useSaveData } from "../../hooks/use-saveDate";
+import { v4 as uuidv4 } from "uuid";
 
 const CadastroPlaca = () => {
   const [file, setFile] = useState(null);
   const [cidade, setCidade] = useState("");
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const { data, setData, saveData } = useSaveData(
+    "http://localhost:3000/cidades"
+  );
 
-    const formData = new FormData();
-    formData.append("file", file);
-    formData.append("cidade", cidade);
+  const combinedJobChangeHandler = (e) => {
+    setCidade(e.target.value);
+    setData({ ...data, cidade: e.target.value, id: uuidv4() });
+  };
 
+  const handleSubmit = () => {
+    return new Promise(async (resolve, reject) => {
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append("cidade", cidade);
+
+      try {
+        const response = await axios.post(
+          "http://localhost:3001/cadastroPlaca",
+          formData
+        );
+        console.log(response.data);
+        resolve(response.data);
+      } catch (error) {
+        console.error("Erro durante o cadastro:", error);
+        reject(error);
+      }
+    });
+  };
+
+  const combinedFormSubmissionHandler = async (e) => {
+    e.preventDefault();
     try {
-      const response = await axios.post(
-        "http://localhost:3000/cadastroPlaca",
-        formData
-      );
-      console.log(response.data);
+      await handleSubmit();
     } catch (error) {
       console.error("Erro durante o cadastro:", error);
+    } finally {
+      saveData();
     }
   };
+
   return (
     <div className={Styles.body}>
       <h1>Envio de Placa</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={combinedFormSubmissionHandler}>
         <label for="file">Imagem da Placa (PNG apenas):</label>
         <input
           type="file"
@@ -36,15 +62,21 @@ const CadastroPlaca = () => {
         <br />
 
         <label for="cidade">Nome da Cidade:</label>
-        <input
-          type="text"
-          value={cidade}
-          onChange={(e) => setCidade(e.target.value)}
-        />
+        <input type="text" value={cidade} onChange={combinedJobChangeHandler} />
         <br />
 
-        <input type="submit" value="Enviar" />
+        <button type="submit">Enviar</button>
       </form>
+
+      <div className={Styles.containerButtons}>
+        <Link to={"/consulta"}>
+          <button>Consultar Placas</button>
+        </Link>
+
+        <Link to={"/relatorio"}>
+          <button>Relatorio Placas</button>
+        </Link>
+      </div>
     </div>
   );
 };
--- FrontEnd/src/routes/routerList.jsx
+++ FrontEnd/src/routes/routerList.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Cadastro from "../pages/Cadastro-Placa/index.jsx";
+import RelatorioPlaca from "../pages/Relatorio-Placa/index.jsx";
 
 export const RouteList = () => {
   return (
     <Routes>
       <Route path="/" element={<Cadastro />} />
+      <Route path="/relatorio" element={<RelatorioPlaca />} />
     </Routes>
   );
 };
