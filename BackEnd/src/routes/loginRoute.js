dbed6f17183f56eb490ce91e44a1670086f3415f
Kurokishin <rafaelpereirasouza15@gmail.com>
2023-10-22 13:18:04 -0300

    fix: utilizando o ID do usuário para gerar o token

--- BackEnd/src/routes/loginRoute.js
+++ BackEnd/src/routes/loginRoute.js
@@ -1,37 +1,37 @@
 const loginRouter = require("express").Router();
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const credencialSchema = require('../models/credencialSchema');
+const credencialSchema = require("../models/credencialSchema");
 
-
+// Verifica se o email e senha são os mesmos do banco de dados e retorna um token para o usuário
 loginRouter.post('/login', async (req, res) => {
-  try {
-    const { email, password } = req.body;
+    try {
+        const { email, password } = req.body;
 
-    // Verifica se o email existe no banco de dados
-    const user = await credencialSchema.findOne({ email });
+        // Verifica se o email existe no banco de dados
+        const user = await credencialSchema.findOne({ email });
 
-    if (!user) {
-      return res.status(401).json({ message: 'Dados incorretos' });
+        if (!user) {
+            return res.status(401).json({ message: 'Dados incorretos' });
+        }
+
+        // Verifica se a senha está correta
+        const passwordMatch = bcrypt.compareSync(password, user.password);
+
+        if (!passwordMatch) {
+            return res.status(401).json({ message: 'Dados incorretos' });
+        }
+
+        // Se o email e a senha estiverem corretos, gera um token JWT
+        const token = jwt.sign({ userId: user._id, email: user.email }, process.env.JWT_SECRET, {
+            expiresIn: '1h', // O token expirará após 1 hora
+        });
+
+        res.status(200).json({ logged: true, token });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ logged: false, message: 'Erro no servidor' });
     }
-
-    // Verifica se a senha está correta
-    const passwordMatch = bcrypt.compareSync(password, user.password);
-
-    if (!passwordMatch) {
-      return res.status(401).json({ message: 'Dados incorretos' });
-    }
-
-    // Se o email e a senha estiverem corretos, gera um token JWT
-    const token = jwt.sign({ email: user.email, password: user.password }, process.env.JWT_SECRET, {
-      expiresIn: 30000 // O token expirará após 30 segundos
-    });
-
-    res.status(200).json({ logged: true, token: token });
-  } catch (error) {
-    console.error(error);
-    res.json({ logged: false, message: 'Erro durante o login' });
-  }
 });
 
 module.exports = loginRouter;
