0aa61b403d66eb85b23a2ccbadfcb4860d87eec0
Luiz Pedro Galdino Silva <luispedrogaldino53@gmail.com>
2023-11-01 08:44:43 -0300

    feat: criação de pastas de login e cadastro

--- FrontEnd/src/routes/routerList.jsx
+++ FrontEnd/src/routes/routerList.jsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
-import Cadastro from "../pages/Cadastro-Placa/index.jsx";
+import CadastroPlaca from "../pages/Cadastro-Placa/index.jsx";
 import RelatorioPlaca from "../pages/Relatorio-Placa/index.jsx";
 import ConsultaPlaca from "../pages/Consulta-Placa/index.jsx";
+import CadastroUsuario from "../pages/Cadastro-Usuario/index.jsx";
+import LoginUsuario from "../pages/Login-Usuario/index.jsx";
 
 export const RouteList = () => {
   return (
     <Routes>
-      <Route path="/" element={<Cadastro />} />
+      <Route path="/" element={<LoginUsuario />} />
+      <Route path="/cadastroUsuario" element={<CadastroUsuario />} />
+      <Route path="/cadadastroPlaca" element={<CadastroPlaca />} />
       <Route path="/relatorio" element={<RelatorioPlaca />} />
       <Route path="/consulta" element={<ConsultaPlaca />} />
     </Routes>
